216a74a22f5c779bd870d7e415b39e4adee06ff9
Denny Scott <Dennyscott301@gmail.com>
2016-05-26 23:41:59 -0500

    Added Logic to events

--- SFMLCourse/main.cpp
+++ SFMLCourse/main.cpp
@@ -10,8 +10,16 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Game");
 	window.setFramerateLimit(60);
 
+	//Stop events from being repeatedly sent on key down
+	window.setKeyRepeatEnabled(false);
+
 	bool play = true;
 	sf::Event event;
+
+	bool isAPressed = false;
+	bool isAReleased = false;
+
+	bool spaceKey = false;
 
 	//Game loop
 	while (play)
@@ -22,22 +30,55 @@ int main()
 		{
 			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::A)
 			{
-				std::cout << "The A key has been pressed\n";
+				isAPressed = true;
 			}
 
 			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::A)
 			{
-				std::cout << "The A key has been released\n";
+				isAReleased = true;
+			}
+
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
+			{
+				spaceKey = true;
+			}
+
+			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Space)
+			{
+				spaceKey = false;
 			}
 
 			if (event.type == sf::Event::Closed)
 			{
+				//Stop the game
 				play = false;
 			}
 		}
 		
 		//LOGIC
+		if (isAPressed)
+		{
+			std::cout << "The A key has been pressed\n";
 
+			isAPressed = false;
+		}
+
+		if (isAReleased)
+		{
+			std::cout << "The A key has been released\n";
+
+			isAReleased = false;
+		}
+
+		if (spaceKey)
+		{
+			std::cout << "SPACE\n";
+		}
+
+		if (spaceKey == false)
+		{
+			std::cout << "NOT SPACE\n";
+		}
 
 		//RENDERING
 		window.clear();
