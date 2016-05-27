3d88cfeb73965a099742158fcc5a3a23982ba02b
Denny Scott <Dennyscott301@gmail.com>
2016-05-26 21:10:23 -0500

    Finshed the initial window

--- SFMLCourse/main.cpp
+++ SFMLCourse/main.cpp
@@ -1,24 +1,14 @@
+//Libraries
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+//Global Variables, functions and classes
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Game");
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
+	std::cin.get();
 
 	return 0;
 }
