ee678e2226299caedddd3836c3b42bcc06497f00
Denny Scott <Dennyscott301@gmail.com>
2016-05-29 19:42:32 -0500

    Added simple movement

--- SFMLCourse/main.cpp
+++ SFMLCourse/main.cpp
@@ -17,41 +17,6 @@ int main()
 	bool play = true;
 	sf::Event event;
 
-	//States for buttons/events
-	bool isAPressed = false;
-	bool isAReleased = false;
-	bool isEnterReleased = false;
-	bool spaceKey = false;
-	bool leftClick = false;
-	bool rightClick = false;
-	bool leftArrow = false;
-	bool rightArrow = false;
-
-	//Variables
-	int numberOfClicks = 0;
-	int mouseX = 0, mouseY = 0;
-	int rectXPosition = 0;
-
-
-	//Images
-	sf::Texture image1;
-	if (image1.loadFromFile("Data/image2.png") == 0)
-	{
-		return 1;
-	}
-
-	//Render shapes
-	sf::RectangleShape rect;
-	rect.setSize(sf::Vector2f(100, 100));
-	rect.setPosition(0, 0);
-	rect.setFillColor(sf::Color::White);
-	rect.setTexture(&image1);
-
-	sf::CircleShape circle;
-	circle.setRadius(50);
-	circle.setPosition(400, 300);
-	circle.setFillColor(sf::Color::Blue);
-
 	//Font
 	sf::Font font;
 	if (font.loadFromFile("Data/arial.ttf") == 0)
@@ -59,35 +24,18 @@ int main()
 		return 1;
 	}
 
-	//Text
-	sf::Text title;
-	title.setFont(font);
-	title.setCharacterSize(30);
-	title.setString("Hello World!");
-	title.setPosition(300, 50);
-	title.setColor(sf::Color::Magenta);
+	//States
+	bool rButton = false;
+	bool leftButton = false, rightButton = false, upButton = false, downButton = false;
 
-	//Sounds
-	sf::SoundBuffer explosionBuffer;
-	if (explosionBuffer.loadFromFile("Data/explosion1.wav") == 0)
-	{
-		return -1;
-	}
+	//Variables
+	int rectRotation = 0;
+	int xVelocity = 0, yVelocity = 0;
 
-	sf::Sound explosion;
-	explosion.setBuffer(explosionBuffer);
-	explosion.setVolume(60);
-
-	//Music
-	sf::Music music;
-	if (music.openFromFile("Data/music.ogg") == 0)
-	{
-		return 1;
-	}
-
-	music.setVolume(5);
-	music.setLoop(true);
-	music.play();
+	//Shapes
+	sf::RectangleShape shape1;
+	shape1.setSize(sf::Vector2f(50, 50));
+	shape1.setPosition(400, 300);
 
 	//Game loop
 	while (play)
@@ -96,136 +44,92 @@ int main()
 
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::A)
-			{
-				explosion.play();
-				isAPressed = true;
-			}
-
-			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::A)
-			{
-				isAReleased = true;
-			}
-
-			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Return)
-			{
-				isEnterReleased = true;
-			}
-
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
-			{
-				spaceKey = true;
-			}
-
-			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Space)
-			{
-				spaceKey = false;
-			}
-
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right)
-			{
-				rightArrow = true;
-			}
-
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Left)
-			{
-				leftArrow = true;
-			}
-
-			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Right)
-			{
-				rightArrow = false;
-			}
-
-			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Left)
-			{
-				leftArrow = false;
-			}
-
-			if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
-			{
-				leftClick = true;
-			}
-
-			if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Right)
-			{
-				rightClick = true;
-			}
-
-			if (event.type == sf::Event::MouseMoved)
-			{
-				mouseX = event.mouseMove.x;
-				mouseY = event.mouseMove.y;
-			}
 
 			if (event.type == sf::Event::Closed)
 			{
 				//Stop the game
 				play = false;
 			}
-		}
-		
-		//LOGIC
-		if (isAPressed)
-		{
-			std::cout << "The A key has been pressed\n";
 
-			isAPressed = false;
-		}
-
-		if (isAReleased)
-		{
-			std::cout << "The A key has been released\n";
-
-			isAReleased = false;
-		}
-
-		if (leftArrow && rightArrow)
-		{
-			std::cout << "Right and Left are Down";
-		}
-
-		if (leftClick)
-		{
-			numberOfClicks++;
-
-			std::cout << "Number of clcks is " << numberOfClicks << "\n";
-
-			if (mouseX < 200)
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R)
 			{
-				play = false;
+				rButton = true;
 			}
 
-			leftClick = false;
+			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::R)
+			{
+				rButton = false;
+			}
+
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::Left)
+					leftButton = true;
+				if (event.key.code == sf::Keyboard::Right)
+					rightButton = true;
+				if (event.key.code == sf::Keyboard::Up)
+					upButton = true;
+				if (event.key.code == sf::Keyboard::Down)
+					downButton = true;
+			}
+
+			if (event.type == sf::Event::KeyReleased)
+			{
+				if (event.key.code == sf::Keyboard::Left)
+					leftButton = false;
+				if (event.key.code == sf::Keyboard::Right)
+					rightButton = false;
+				if (event.key.code == sf::Keyboard::Up)
+					upButton = false;
+				if (event.key.code == sf::Keyboard::Down)
+					downButton = false;
+			}
 		}
 
-		if (rightClick)
+
+		//LOGIC
+		if (rButton)
 		{
-			numberOfClicks--;
-
-			std::cout << "Number of clcks is " << numberOfClicks << "\n";
-
-			rightClick = false;
+			rectRotation++;
+			shape1.setRotation(rectRotation);
 		}
 
-		if (isEnterReleased)
-		{
-			std::cout << "Enter has been released";
+		//Velocity
+		//Xaxis
+		if (rightButton)
+			xVelocity = 5;
 
-			isEnterReleased = false;
-		}
+		if (leftButton)
+			xVelocity = -5;
 
-		std::cout << "Mouse x: " << mouseX << " Mouse y: " << mouseY << "\n";
+		if (leftButton && rightButton)
+			xVelocity = 0;
+		
+		if (!leftButton && !rightButton)
+			xVelocity = 0;
 
-		rectXPosition++;
-		rect.setPosition(rectXPosition, rectXPosition);
+		//YAxis
+		if (upButton)
+			yVelocity = -5;
+		
+		if (downButton)
+			yVelocity = 5;
+
+		if (upButton && downButton)
+			yVelocity = 0;
+
+		if (!upButton && !downButton)
+			yVelocity = 0;
+
+		//Move
+		shape1.move(xVelocity, yVelocity);
+
+		//Collision
 
 		//RENDERING
 		window.clear();
 
-		window.draw(rect);
-		window.draw(circle);
-		window.draw(title);
+		window.draw(shape1);
 
 		window.display();
 	}
